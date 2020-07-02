3d621ea2df8c25b086aeaa498fb8e7c092c0748c
webturing <zj@webturing.com>
2020-07-02 17:39:53 +0800

    利用数据库保存计算结果，优化计算

--- app/src/ai/SolveServlet.java
+++ app/src/ai/SolveServlet.java
@@ -7,9 +7,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.util.Arrays;
+import java.sql.*;
 
 @WebServlet("/solve")
 public class SolveServlet extends HttpServlet {
+    private String diverClass;
+    private String userName;
+    private String password;
+    private String url;
+    private Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        diverClass = /* getServletConfig(). */getServletContext().getInitParameter("driver");
+        userName = /* getServletConfig(). */getServletContext().getInitParameter("username");
+        password = /* getServletConfig(). */getServletContext().getInitParameter("password");
+        url = /* getServletConfig(). */getServletContext().getInitParameter("url");
+
+        try {
+            Class.forName(diverClass);
+            connection = DriverManager.getConnection(url, userName, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int a = 0;
@@ -25,11 +50,61 @@ public class SolveServlet extends HttpServlet {
         } catch (Exception e) {
 
         }
-        String result = ai.Engine.solve24(a, b, c, d);
+        int[] arr = new int[]{a, b, c, d};
+        Arrays.sort(arr);
+        String key = Arrays.toString(arr);
+        String result = query(key);
+        if (result == null) {
+            System.err.println("重新计算结果并写入数据库");
+            result = ai.Engine.solve24(a, b, c, d);
+            save(key, result);
+        }
 
-        request.getSession().setAttribute("result",result);
+        request.getSession().setAttribute("result", result);
         response.sendRedirect("index.jsp");
 
+    }
+
+    private boolean save(String key, String result) {
+        try {
+            Statement stmt = connection.createStatement();
+            PreparedStatement ps = connection.prepareStatement("insert into point24(numbers,solution) value(?,?)");
+
+            ps.setString(1, key);
+            ps.setString(2, result);
+            int x = ps.executeUpdate();
+            System.err.println("写入数据库成功");
+            return x > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("数据库链接失败");
+            return false;
+        }
+    }
+
+    private String query(String key) {
+        String ans = null;
+
+        try {
+            Statement stmt = connection.createStatement();//key
+            PreparedStatement ps = connection.prepareStatement("select solution from point24 where numbers=? LIMIT 1");
+            ps.setString(1, key);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                ans = rs.getString(1);
+                System.err.println("数据已经存在，直接读取数据库");
+
+            }
+
+            rs.close();
+            stmt.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("数据库链接失败");
+        }
+        return ans;
     }
 
     @Override
